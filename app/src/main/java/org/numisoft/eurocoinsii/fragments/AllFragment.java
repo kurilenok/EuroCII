66dc72ce42aa27b63d51f6f74796105759888acd
kurilenok <kurilenok@gmail.com>
2016-12-15 12:28:11 -0600

    first

---
+++ app/src/main/java/org/numisoft/eurocoinsii/fragments/AllFragment.java
@@ -0,0 +1,20 @@
+package org.numisoft.eurocoinsii.fragments;
+
+import android.annotation.SuppressLint;
+
+import org.numisoft.eurocoinsii.models.Theme;
+
+/**
+ * Created by kukolka on 22.08.16.
+ */
+@SuppressLint("ValidFragment")
+public class AllFragment extends BasicFragment {
+
+    public AllFragment(Theme theme1, Theme theme2) {
+        super(theme1, theme2);
+    }
+
+    public AllFragment() {
+        super();
+    }
+}
---
+++ app/src/main/java/org/numisoft/eurocoinsii/fragments/NeedFragment.java
@@ -0,0 +1,20 @@
+package org.numisoft.eurocoinsii.fragments;
+
+import android.annotation.SuppressLint;
+
+import org.numisoft.eurocoinsii.models.Theme;
+
+/**
+ * Created by kukolka on 10/18/2016.
+ */
+@SuppressLint("ValidFragment")
+public class NeedFragment extends BasicFragment{
+
+    public NeedFragment(Theme theme1, Theme theme2) {
+        super(theme1, theme2);
+    }
+
+    public NeedFragment() {
+        super();
+    }
+}
